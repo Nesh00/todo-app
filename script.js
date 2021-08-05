92287d2d60a1e4ddf6285d3327eb773bc7cbe4c4
nenad <c.nenad00@gmail.com>
2021-08-05 22:28:57 +0100

    render input value in list

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const themeBtn = document.querySelector('#theme-btn');
 const themeImg = document.querySelector('#theme-img');
 const checkBtn = document.querySelector('.radio-btn');
+const listGroup = document.querySelector('.list-group');
+const inputForm = document.querySelector('.form-control');
 
 const changeThemeImg = () => {
   if (document.documentElement.className === 'theme-light')
@@ -33,4 +35,29 @@ themeBtn.addEventListener('click', toggleTheme);
 
 checkBtn.addEventListener('click', function (e) {
   this.classList.toggle('radio-btn--checked');
+});
+
+const renderListItem = function (input) {
+  const html = `
+    <li class="list-group-item list-item-bg d-flex align-items-center">
+        <div class="radio-btn align-self-center"></div>
+        <div class="list-text">${input.trim()}</div>
+
+        <button class="d-lg-none close-btn align-self-start ms-auto">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+            <path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" />
+        </svg>
+        </button>
+    </li>
+    `;
+  listGroup.insertAdjacentHTML('afterbegin', html);
+};
+
+inputForm.addEventListener('keydown', function (e) {
+  const input = e.target;
+
+  if (e.key === 'Enter') {
+    renderListItem(input.value);
+    input.value = '';
+  }
 });
